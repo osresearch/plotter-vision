addd646c9ea57294f01b7e68873edac2749bdb65
Trammell hudson <hudson@trmm.net>
2020-12-06 18:09:01 +0100

    fix missing lines when triangle and segment share a point (#9)

--- hidden.js
+++ hidden.js
@@ -2,6 +2,9 @@
  * Given a triangle and a line segment in screen coordintes,
  * determine if the line segment is occluded by the triangle.
  *
+ * Negative Z distance is behind the camera,
+ * Increasing Z is further away
+ *
  * Returns:
  */
 const tri_no_occlusion = 0; // no occlusion and processing should continue
@@ -9,7 +12,7 @@ const tri_in_front = 1; // no occlusion and processing should stop
 const tri_hidden = 2; // occlusion and the segment is totally hidden
 const tri_clipped = 3; // occlusion and either p0 or p1 has been updated
 const tri_split = 4; //  occlusion and p0/p1 have been updated and p2/p3 have been created
-const EPS = 0.00001;
+const EPS = 0.001;
 
 let p_max = null;
 let p_min = null;
@@ -32,24 +35,21 @@ function occlude(t,s)
 	if (!p_max) p_max = createVector();
 	if (!p_min) p_min = createVector();
 
-	v3max(p_max, s.p0,s.p1);
-	v3min(p_min, s.p0,s.p1);
+	v3max(p_max, s.p0, s.p1);
+	v3min(p_min, s.p0, s.p1);
 
 	// if the segment max z is closer than the minimum
 	// z of the triangle, then this triangle can not occlude
 	if (p_max.z <= t.min.z)
-		return tri_in_front;
+		return tri_no_occlusion;
 
-	// perform a bounding box check for the triangle min/max.
+	// perform a screen coordinates bounding box check for the
+	// triangle min/max.
 	// if the segment lies outside of this box and doesn't
 	// cross it, then there is no chance of occlusion
-	if (p_min.x < t.min.x && p_max.x < t.min.x)
+	if (p_max.x < t.min.x || t.max.x < p_min.x)
 		return tri_no_occlusion;
-	if (p_min.y < t.min.y && p_max.y < t.min.y)
-		return tri_no_occlusion;
-	if (p_min.x > t.max.x && p_max.x > t.max.x)
-		return tri_no_occlusion;
-	if (p_min.y > t.max.y && p_max.y > t.max.y)
+	if (p_max.y < t.min.y || t.max.y < p_min.y)
 		return tri_no_occlusion;
 
 	// there is a chance this segment crosses the triangle,
@@ -64,7 +64,10 @@ function occlude(t,s)
 	{
 		// if the segment z is closer than the triangle z
 		// then the segment is in front of the triangle
-		if (s.p0.z < tp0.z && s.p1.z < tp1.z)
+		// equality check in case the segment shares a vertex
+		// with the triangle.  If it is coming towards the
+		// camera in the other point, the no occlusion.
+		if (s.p0.z <= tp0.z && s.p1.z <= tp1.z)
 			return tri_no_occlusion;
 
 		// if the barycentric coord is 0 on the same edge
